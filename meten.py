8d275d0cdacc243ccff9f11f79e0740479a58389
Bilal094 <99066318@mydavinci.nl>
2021-09-13 13:29:07 +0200

    min en max

--- meten.py
+++ meten.py
@@ -3,8 +3,10 @@ b = int(input('Type een geheel getal voor b '))
 if a > b:
     max = a
     print('a is het grootste getal: '+ str(max))
+    print('Het maximum is: '+ str(max))
 elif a < b:
     min = a
     print('a is het kleinste getal: '+ str(min))
+    print('Het minimum is: '+ str(min))
 else:
     print('a is gelijk aan b')
