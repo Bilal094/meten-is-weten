4ce362b3bd2ac0775bf2772dc9fe860e4f3a3476
Bilal094 <99066318@mydavinci.nl>
2021-09-13 13:22:21 +0200

    elif-statement

--- meten.py
+++ meten.py
@@ -3,3 +3,6 @@ b = int(input('Type een geheel getal voor b '))
 if a > b:
     max = a
     print('a is het grootste getal: '+ str(max))
+elif a < b:
+    min = a
+    print('a is het kleinste getal: '+ str(min))
